9258b05b2a7a0c4f258eddd79afd73f0d72eae80
Rana_Hamza <descent.hamza153@gmail.com>
2024-10-01 11:57:34 +0500

    updated

--- Api_dev_begineer/database.py
+++ Api_dev_begineer/database.py
@@ -1,9 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
+from config import settings
 
 # SQLALCHEMY_DATABASE_URL = 'postgresql://<username>:<password>@localhost/<database>'
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Pk135430@localhost/fastapi'
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Pk135430@localhost:5432/fastapi'
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
@@ -19,3 +24,14 @@ def get_db():
     finally:
         db.close() 
 
+while True:
+    try:
+        conn = psycopg2.connect(host="localhost",database="fastapi",user="postgres",
+                                password="Pk135430",cursor_factory=RealDictCursor)
+        curr = conn.cursor()
+        print("Database connected successfully")
+        break
+    except Exception as e:
+        print("Database connection error")
+        print(f"Error: {e}")
+        time.sleep(5)
--- Api_dev_begineer/main.py
+++ Api_dev_begineer/main.py
@@ -1,65 +1,26 @@
-from typing import Optional
-from fastapi import FastAPI , Body , Response ,status ,HTTPException , Depends
-from pydantic import BaseModel
-from random import randrange
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import time
+from fastapi import FastAPI 
 from passlib.context import CryptContext
-from sqlalchemy.orm import Session
-import utils
-import models , schemas 
+import models
 from models import *
 from schemas import *
 from utils import *
-from database import engine , get_db
-from routers import post , user , auth
+from database import engine 
+from routers import post , user , auth , vote
 # from main import app
+from config import settings
+
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-
-while True:
-    try:
-        conn = psycopg2.connect(host="localhost",database="fastapi",user="postgres",
-                                password="Pk135430",cursor_factory=RealDictCursor)
-        curr = conn.cursor()
-        print("Database connected successfully")
-        break
-    except Exception as e:
-        print("Database connection error")
-        print(f"Error: {e}")
-        time.sleep(5)
-
-
-
-new_posts = [{"title": "post1", "content": "content1","id":1},
-             {"title": "post2", "content": "content2","id":2}]
-
-def find_post(id):
-    for p in new_posts:
-        if p['id'] == id:
-            return p
-        
-def find_index_post(id):
-    for i, p in enumerate(new_posts):
-        if p['id'] == id:
-            return i
-    return None
-
-
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
+app.include_router(vote.router)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-@app.post("/items/")
-def create_item():
-    return {"hello" : "world"}
 
